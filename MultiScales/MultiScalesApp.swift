455e2ef4c388a723ef0bcae4f886799b36d478bf
Brio <info@briomusic.net>
2023-11-01 18:30:03 +0100

    Initial Commit

---
+++ MultiScales/MultiScalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiScalesApp.swift
+//  MultiScales
+//
+//  Created by Brio Taliaferro on 01.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MultiScalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
